947132c5a9c8a5d3aef785c4316d5ab40672fd27
Dung Trung <87816726+dungdg1901@users.noreply.github.com>
2021-12-03 10:02:54 +0700

    player movement

---
+++ SE06-19.2/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 3.5f;
+    [SerializeField]
+    private float _gravity = 9.81f;
+    [SerializeField]
+    private float _jumpHeight = 3.5f;
+
+    private float _directionY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        calculateMovement();
+    }
+
+    void calculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Space");
+            _directionY = _jumpHeight;
+        }
+
+        _directionY -= _gravity * Time.deltaTime;
+
+        direction.y = _directionY;
+        direction = transform.transform.TransformDirection(direction);
+        _controller.Move(direction * _speed * Time.deltaTime);
+    }
+}
