c0dd91f82da9bc1046bdaedd6af04bddd0cb67f3
Dung Trung <87816726+dungdg1901@users.noreply.github.com>
2021-12-08 19:49:57 +0700

    create menu manager

--- SE06-19.2/Assets/Scripts/Server/Launcher.cs
+++ SE06-19.2/Assets/Scripts/Server/Launcher.cs
@@ -19,6 +19,7 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     public override void OnJoinedLobby()
     {
+        MenuManager.Instance.OpenMenu("title");
         Debug.Log("joined lobby");
     }
 }
