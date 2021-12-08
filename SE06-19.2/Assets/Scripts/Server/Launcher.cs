338f5f56ad18206bb8dc486293cc2991864897ce
Dung Trung <87816726+dungdg1901@users.noreply.github.com>
2021-12-08 19:32:58 +0700

    connect to server
    connect to server and join lobby for creating rooms, making games, etc..

---
+++ SE06-19.2/Assets/Scripts/Server/Launcher.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("connecting to master");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+        Debug.Log("connected to master");
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("joined lobby");
+    }
+}
