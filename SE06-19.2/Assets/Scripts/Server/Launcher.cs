97999f496d1cecc5c71d171ce5d882effced83c0
Dung Trung <87816726+dungdg1901@users.noreply.github.com>
2021-12-08 22:47:52 +0700

    find room feature

--- SE06-19.2/Assets/Scripts/Server/Launcher.cs
+++ SE06-19.2/Assets/Scripts/Server/Launcher.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
+
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    public static Launcher Instance;
+
     [SerializeField] TMP_InputField roomNameInputField;
     [SerializeField] TMP_Text errorText;
     [SerializeField] TMP_Text roomNameText;
+    [SerializeField] Transform roomListContent;
+    [SerializeField] GameObject roomListItemPrefabs;
 
+    private void Awake()
+    {
+        Instance = this;
+    }
     private void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
@@ -56,8 +66,26 @@ public class Launcher : MonoBehaviourPunCallbacks
         MenuManager.Instance.OpenMenu("loading");
     }
 
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
     public override void OnLeftRoom()
     {
         MenuManager.Instance.OpenMenu("title");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach(Transform trans in roomListContent)
+        {
+            Destroy(trans.gameObject);
+        }
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Instantiate(roomListItemPrefabs, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+        }
     }
 }
