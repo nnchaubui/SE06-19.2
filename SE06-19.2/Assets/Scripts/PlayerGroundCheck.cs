e574220109c13ab16a88b9ff36059113429489c5
Dung Trung <87816726+dungdg1901@users.noreply.github.com>
2021-12-09 15:53:31 +0700

    remove some no-needed files and finish player movement

---
+++ SE06-19.2/Assets/Scripts/PlayerGroundCheck.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGroundCheck : MonoBehaviour
+{
+    PlayerController playerController;
+    private void Awake()
+    {
+        playerController = GetComponentInParent<PlayerController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == playerController.gameObject)
+        {
+            return;
+        }
+        playerController.setGroundedState(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == playerController.gameObject)
+        {
+            return;
+        }
+        playerController.setGroundedState(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject == playerController.gameObject)
+        {
+            return;
+        }
+        playerController.setGroundedState(true);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == playerController.gameObject)
+        {
+            return;
+        }
+        playerController.setGroundedState(true);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject == playerController.gameObject)
+        {
+            return;
+        }
+        playerController.setGroundedState(false);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject == playerController.gameObject)
+        {
+            return;
+        }
+        playerController.setGroundedState(true);
+    }
+}
